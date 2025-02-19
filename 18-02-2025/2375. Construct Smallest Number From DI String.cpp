ab362af4d94f04b5451a33d35f56b0d0a33c88c3
Shashank pandey <126805929+HYDRO2070@users.noreply.github.com>
2025-02-19 21:26:26 +0530

    Create 2375. Construct Smallest Number From DI String.cpp

---
+++ 18-02-2025/2375. Construct Smallest Number From DI String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        string result;
+        stack<int> stk;
+        int num = 1;
+
+        for (int i = 0; i <= pattern.size(); i++) {
+            stk.push(num++);
+            
+            if (i == pattern.size() || pattern[i] == 'I') {
+                while (!stk.empty()) {
+                    result += to_string(stk.top());
+                    stk.pop();
+                }
+            }
+        }
+        return result;
+    }
+};
